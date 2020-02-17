d56d4fde6899d31ea0a76bc343da594ff9ff90ac
Aaron Rodriguez <aaroner@yahoo.com>
2020-02-17 17:45:56 -0600

    14/23

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const { ApolloServer, gql } = require('apollo-server');
+const { GraphQLScalarType } = require('graphql');
+const { Kind } = require('graphql/language');
 
 const typeDefs = gql`
+  scalar Date
+
   enum Status {
     PLAYED
     INTERESTED
@@ -16,7 +20,7 @@ const typeDefs = gql`
   type Game {
     id: ID!
     title: String!
-    releaseDate: String
+    releaseDate: Date
     genre: String
     status: Status
     dev: [Dev] # Valid null, [], [...some data], X not valid [...some data w/o name or id]
@@ -30,23 +34,42 @@ const typeDefs = gql`
     games: [Game]
     game(id: ID): Game
   }
+
+  type Mutation {
+    addGame(title: String, releaseDate: Date, id: ID): [Game]
+  }
 `;
+
+const devs = [
+  {
+    id: 'shigeru',
+    name: 'Shigeru Miyamoto',
+  },
+  {
+    id: 'makoto',
+    name: 'Makoto Kanoh',
+  },
+];
 
 const games = [
   {
     id: 'assdqwdas',
     title: 'Super Mario World',
-    releaseDate: '11-21-1990',
+    releaseDate: new Date('11-21-1990'),
+    dev: [
+      {
+        id: 'shigeru',
+      },
+    ],
   },
   {
     id: 'assdqwdfw',
     title: 'Super Metroid',
-    releaseDate: '03-19-1994',
+    releaseDate: new Date('03-19-1994'),
     genre: 'Action / Side Scrolling',
     dev: [
       {
-        id: 'asdawdawd',
-        name: 'Shigeru Miyamoto',
+        id: 'makoto',
       },
     ],
   },
@@ -64,6 +87,50 @@ const resolvers = {
       return foundGame;
     },
   },
+  Game: {
+    dev: (obj, arg, context) => {
+      // db call
+      const devIds = obj.dev.map(dev => dev.id);
+      const filteredDevs = devs.filter(dev => {
+        return devIds.includes(dev.id);
+      });
+      return filteredDevs;
+    },
+  },
+  Mutation: {
+    addGame: (obj, { id, title, releaseDate }, context) => {
+      // mutation / db stuff
+      const newGamesList = [
+        ...games,
+        // new game data
+        {
+          id,
+          title,
+          releaseDate,
+        },
+      ];
+      // return data as expected in schema
+      return newGamesList;
+    },
+  },
+  Date: new GraphQLScalarType({
+    name: 'Date',
+    description: "It's a date",
+    parseValue(value) {
+      // value from the client
+      return new Date(value);
+    },
+    serialize(value) {
+      //  the value sent to the client
+      return value.getTime();
+    },
+    parseLiteral(ast) {
+      if (ast.kind === Kind.INT) {
+        return new Date(ast.value);
+      }
+      return null;
+    },
+  }),
 };
 
 const server = new ApolloServer({
